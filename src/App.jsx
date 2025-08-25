c88c1bfbdb813c40754bc406df97f7e3bb398b0b
Rahul <talwarrahul295@gmail.com>
2025-08-26 00:44:42 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import EventDetail from "./pages/EventDetail";
 import Header from "./components/Header";
 import { Toaster } from "react-hot-toast";
 import ComparePage from "./pages/ComparePage";
+import { useAuth0 } from "@auth0/auth0-react";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
+  const {user} = useAuth0();
   return (
     <div className="p-5 bg-gradient-to-br from-black via-indigo-950  to-blue-900 min-h-screen text-white">
       <Header />
 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/event/:id" element={<EventDetail />} />
-          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/" element={user ? <HomePage /> : <SignupPage/>} />
+          <Route path="/event/:id" element={user ? <EventDetail /> : <SignupPage/>} />
+          <Route path="/compare" element={user ? <ComparePage/> : <SignupPage/>} />
         </Routes>
       </BrowserRouter>
 
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+function SignupPage() {
+  return (
+    <div className="text-center mt-20">
+        <h1 className="text-5xl font-mono">Signup to view this page </h1>
+      
+    </div>
+  )
+}
+
+export default SignupPage
